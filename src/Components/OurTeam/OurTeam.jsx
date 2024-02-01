7c5b80cdf0192c8ee0e8619d45210ab45d9406cf
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2024-02-01 11:12:30 +0600

    [DONE] information of new member added in our team component

--- src/Components/OurTeam/OurTeam.jsx
+++ src/Components/OurTeam/OurTeam.jsx
@@ -2,6 +2,7 @@ import shoyas from "../../assets/team/shoyas-2.webp";
 import shagor from "../../assets/team/Rakibulislam1.webp";
 import zehad from "../../assets/team/zehad.webp";
 import bitue from "../../assets/team/bitue.webp";
+import rifad from "../../assets/team/rifadul.webp";
 import LinkedinIcon from "../icons/LinkedinIcon";
 import { Link } from "react-router-dom";
 import GitHubIcon from "../icons/GitHubIcon";
@@ -148,6 +149,41 @@ const OurTeam = () => {
               target="_blank"
               className="p-2"
             >
+              <PortfolioIcon />
+            </Link>
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-center">
+            <img className="rounded-full w-52 h-52" src={rifad} alt={rifad} />
+          </div>
+          <div className="text-center">
+            <h4 className="text-xl font-semibold mt-5 text-[#453F41]">
+              Md Rifadul Islam Rifad
+            </h4>
+            <p className="text-base mt-1 text-[#453F41]">
+              MERN Stack Developer
+            </p>
+          </div>
+          <div className="flex justify-center mt-3">
+            <Link
+              to="https://www.linkedin.com/in/md-rifadul-islam-rifad-90aa612a5/"
+              target="_blank"
+              className="p-2"
+            >
+              <LinkedinIcon />
+            </Link>
+            <Link
+              to="https://github.com/RF-Rifat"
+              target="_blank"
+              className="p-2"
+            >
+              <GitHubIcon />
+            </Link>
+            <Link to="_" target="_blank" className="p-2">
+              <EmailIcon />
+            </Link>
+            <Link to="_" target="_blank" className="p-2">
               <PortfolioIcon />
             </Link>
           </div>
